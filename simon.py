3d73e760dd20a1fb7da53c61c9d096e19debac44
DESKTOP-ECTOU6J\thana <thanayut.165022@gmail.com>
2019-09-21 00:12:49 +0700

    try to build function to check the answer

--- simon.py
+++ simon.py
@@ -9,15 +9,15 @@ root.geometry("1080x750")
 
 
 #header label
-headlb = 'Press A key to Start'
+headlb = 'Level'
 label1 = tk.Label(root,text=headlb,font="arial", width = len(headlb),bg="yellow")
 label1.pack()
 #End-header label
 
-
+#load image----------------------------------------------------------------------
 img0 = Image.open("images/thuglife.png")
 photo0 = ImageTk.PhotoImage(img0)
-#load image
+
 img1 = Image.open("images\img1.png")
 photo1 = ImageTk.PhotoImage(img1)
 
@@ -33,8 +33,7 @@ photo4 = ImageTk.PhotoImage(img4)
 img5 = Image.open("images\img4ff.png")
 photo5 = ImageTk.PhotoImage(img5)
 
-#End-load image
-
+#-----------------------------------------------------------------------
 #label with img
 limg1 = tk.Label(root,image=photo1)
 limg1.place(x=130,y=50)
@@ -47,6 +46,7 @@ limg3.place(x=130,y=400)
 
 limg4 = tk.Label(root,image=photo4)
 limg4.place(x=610,y=400)
+#-----------------------------------------------------------------------
 
 allimg = [limg1,limg2,limg3,limg4]
 photo = [photo1,photo2,photo3,photo4]
@@ -55,7 +55,7 @@ status = False
 selection = []
 pattern = []
 
-
+#-----------------------------------------------------------------------
 def gamestart_animate(idx =0):  
     a = random.randrange(len(allimg))
     pattern.append("img"+str(a))
@@ -69,65 +69,39 @@ def gamestart_animate(idx =0):
         limg2.bind("<1>", select_img2)
         limg3.bind("<1>", select_img3)
         limg4.bind("<1>", select_img4)
-    
-
+#-----------------------------------------------------------------------
 def select_img1(self):
     limg1.config(image=photo5)
     root.after(500,lambda:limg1.config(image=photo1))
     selection.append('img0')
-    if len(pattern)-1 == len(selection):
-        pass
-    elif pattern == selection:
+    if pattern == selection:
         root.after(1500,next_pp)
-    else:
-        print("กาก")
-
 
 def select_img2(self):
     limg2.config(image=photo5)
     root.after(500,lambda:limg2.config(image=photo2))
     selection.append('img1')
-    if len(pattern)-1 == len(selection):
-        pass
-    elif pattern == selection:
+    if pattern == selection:
         root.after(1500,next_pp)
-    else:
-        print("กาก")
-
 
 def select_img3(self):
     limg3.config(image=photo5)
     root.after(500,lambda:limg3.config(image=photo3))
     selection.append('img2')
-    if len(pattern)-1 == len(selection):
-        pass
-    elif pattern == selection:
+    if pattern == selection:
         root.after(1500,next_pp)
-    else:
-        print("กาก")
-
 
 def select_img4(self):
     limg4.config(image=photo0)
     root.after(500,lambda:limg4.config(image=photo4))
     selection.append('img3')
-    if len(pattern)-1 == len(selection):
-        pass
-    elif pattern == selection:
+    if pattern == selection:
         root.after(1500,next_pp)
-    else:
-        print("กาก")
-
-
-# ล่าสุดคือเพิ่ม selection = [] ใส่ฟัง่ชั่น ข้างบน
-    
+#-----------------------------------------------------------------------
 def showscore():
     print("userclick:"+str(selection))
     print("pattern:"+str(pattern))
-    
-
-root.after(2000,gamestart_animate)
-#Key press to start fn
+#-----------------------------------------------------------------------
 def next_pp():
     if pattern == selection:
         a = random.randrange(len(allimg))
@@ -137,7 +111,13 @@ def next_pp():
     else:
         pass
     selection.clear()
+#-----------------------------------------------------------------------
+def checkAns():
+    if pattern != selection:
+        print("try again")
 
+
+root.after(2000,gamestart_animate)
 
 btn = tk.Button(root,text="SHOW SCORE",command=showscore).pack(side='bottom')
 
@@ -146,12 +126,7 @@ btn = tk.Button(root,text="SHOW SCORE",command=showscore).pack(side='bottom')
 
 
 #root.bind("<Key>",key)
-#End-Key press to start fn
 
-# limg1.bind('<1>', keyimg1)
-# limg2.bind('<1>', keyimg2)
-# limg3.bind('<1>', keyimg3)
-# limg4.bind('<1>', keyimg4)
 
 
 
