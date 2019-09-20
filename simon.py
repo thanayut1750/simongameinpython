6ab5680a3acb0ea08f0cb643003d4961b0e4d5ba
DESKTOP-ECTOU6J\thana <thanayut.165022@gmail.com>
2019-09-20 15:22:24 +0700

    2 day left

--- simon.py
+++ simon.py
@@ -50,59 +50,79 @@ allimg = [limg1,limg2,limg3,limg4]
 photo = [photo1,photo2,photo3,photo4]
 
 status = False
-selection = ''
+selection=[]
 pattern = []
 
-print(pattern)
 
 def animate(idx =0):
-
+    
     a = random.randrange(len(allimg))
+    pattern.append("img"+str(a))
     allimg[a].config(image=photo5)
     root.after(500,lambda:allimg[a].config(image=photo[a]))
     idx += 1
     if idx < len(allimg)-2:
         root.after(1000,lambda:animate(idx))
+        
     else:
         limg1.bind("<1>", select_img1)
         limg2.bind("<1>", select_img2)
         limg3.bind("<1>", select_img3)
         limg4.bind("<1>", select_img4)
+    
 
 def select_img1(self):
     limg1.config(image=photo5)
     root.after(500,lambda:limg1.config(image=photo1))
-    pattern.append('img1')
+    selection.append('img0')
+    
 
 
 def select_img2(self):
     limg2.config(image=photo5)
     root.after(500,lambda:limg2.config(image=photo2))
-    pattern.append('img2')
+    selection.append('img1')
+    next_pp()
+    limg2.unbind('<1>')
 
 
 def select_img3(self):
     limg3.config(image=photo5)
     root.after(500,lambda:limg3.config(image=photo3))
-    pattern.append('img3')
+    selection.append('img2')
+    next_pp()
+    limg3.unbind('<1>')
 
 
 def select_img4(self):
     limg4.config(image=photo5)
     root.after(500,lambda:limg4.config(image=photo4))
-    pattern.append('img4')
+    selection.append('img3')
+    next_pp()
+    limg4.unbind('<1>')
 
-def checkans():
-     
 
+# def checkans():
+    
+def showscore():
+    print("userclick:"+str(selection))
+    print("pattern:"+str(pattern))
 
+score = showscore()
 root.after(2000,animate)
 #Key press to start fn
-def keyimg1(event):
-    limg1["image"] = photo5
-    limg1.after(300,func=animation1)
-def animation1():
-    limg1["image"] = photo1
+def next_pp():
+    if pattern == selection:
+        a = random.randrange(len(allimg))
+        allimg[a].config(image=photo5)
+        root.after(500,lambda:allimg[a].config(image=photo[a]))
+        pattern.append("img"+str(a))
+    else:
+        print("กาก")
+
+
+btn = tk.Button(root,text="SHOW SCORE",command=showscore).pack(side='bottom')
+
 
 
 
